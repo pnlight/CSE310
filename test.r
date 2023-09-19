049ab02114dde0e415a1079c93eda2284a2087f2
Niko Ruivivar <pnlight@gmail.com>
2023-09-19 09:39:48 -0600

    update

--- test.r
+++ test.r
@@ -1,71 +1,38 @@
-# Load libraries
-library(quantmod)
+---
+title: "Stocks"
+format:
+  html:
+    code-link: true
+    code-fold: true
+editor: visual
+execute:
+  keep-md: true
+---
+
+```{r setup, include=FALSE}
+knitr::opts_chunk$set(warning = FALSE, message = FALSE) 
+
+library(ggplot2)
+library(tidyverse)
 library(tidyquant)
-library(forecast)
-library(ggplot2)  # Load ggplot2 for plotting
+library(dygraphs)
+library(ggforce)
+library(lubridate)
+library(timetk)
+```
 
-# Function to retrieve historical stock price data for NVDA
-get_nvda_stock_data <- function(start_date, end_date) {
-  symbol <- "NVDA"
-  # Use quantmod to retrieve NVDA stock data
-  data <- quantmod::getSymbols(symbol, from = start_date, to = end_date, auto.assign = FALSE)
-  return(data)
-}
+```{r}
+df <- tq_get(c("NVDA", "TWTR", "IBM"),
+             get = "stock.prices",
+             from = "2017-12-01",
+             to   = "2022-12-01")
 
-# Function to perform EDA and data preprocessing
-perform_eda <- function(stock_data) {
-  # Placeholder: Implement data preprocessing here
-  # For example, you can calculate moving averages or remove missing values
-  # Return the preprocessed data
-  preprocessed_data <- stock_data  # Placeholder; modify this as needed
-  return(preprocessed_data)
-}
+df2 <- df %>% pivot_wider(names_from = symbol, values_from = adjusted, id_cols = date)
 
-# Function to train and evaluate forecasting model
-train_and_evaluate_model <- function(preprocessed_data) {
-  # Split data into training and testing sets
-  # Train your forecasting model here (e.g., using ARIMA or other methods)
-  # Evaluate the model's performance
-  
-  # Placeholder for model evaluation results
-  model_evaluation_results <- list(
-    metric1 = 0.0,  # Replace with actual metrics
-    metric2 = 0.0   # Replace with actual metrics
-  )
-  
-  return(model_evaluation_results)
-}
+xts <- df2 %>%
+  tk_xts(date = date)
 
-# Function to plot the time series
-plot_time_series <- function(preprocessed_data) {
-  # Create a time series plot using ggplot2
-  ggplot(data = preprocessed_data, aes(x = index(preprocessed_data), y = Adjusted)) +
-    geom_line() +
-    labs(x = "Date", y = "Stock Price", title = "NVDA Stock Price Time Series") +
-    theme_minimal()
-}
+stocks <- dygraph(xts, main = "Tech Stocks") %>%
+  dyRangeSelector(dateWindow = c("2017-12-01", "2022-12-01"))
 
-# Main function
-main <- function() {
-  # Define date range
-  start_date <- "2020-01-01"
-  end_date <- "2021-12-31"
-  
-  # Retrieve NVDA stock data
-  nvda_stock_data <- get_nvda_stock_data(start_date, end_date)
-  
-  # Perform EDA and data preprocessing
-  preprocessed_data <- perform_eda(nvda_stock_data)
-  
-  # Plot the time series
-  plot_time_series(preprocessed_data)
-  
-  # Train and evaluate the forecasting model
-  model_evaluation_results <- train_and_evaluate_model(preprocessed_data)
-  
-  # Display output
-  # Display model_evaluation_results here or in a separate function
-}
-
-# Run the main function
-main()
+stocks
